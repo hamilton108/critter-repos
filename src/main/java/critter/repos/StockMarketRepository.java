2e9f5e72bafca5d4b426ccd2ba32c30959e11964
Rolf C Stadheim <hamilton@hamilton.com>
2022-09-29 12:01:23 +0200

    Add methods to StockMarketRepository: activePurchasesWithCritters,purchasesWithSalesAll

--- src/main/java/critter/repos/StockMarketRepository.java
+++ src/main/java/critter/repos/StockMarketRepository.java
@@ -2,10 +2,14 @@ package critter.repos;
 
 import critter.stock.Stock;
 import critter.stockoption.StockOption;
+import critter.stockoption.StockOptionPurchase;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StockMarketRepository<T,T2> {
     Stock findStock(T stockInfo);
     Optional<StockOption> findStockOption(T2 stockOptInfo);
+    List<StockOptionPurchase> activePurchasesWithCritters(int purchaseType);
+    List<StockOptionPurchase> purchasesWithSalesAll(int purchaseType, int status, StockOption.OptionType ot);
 }
